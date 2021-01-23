df7ab5c5c7c2f02411bd2a2a14a320b3b5b8c1c0
PatoMo <73911080+PatoMo@users.noreply.github.com>
2021-01-23 16:15:52 -0300

    Add files via upload
    Se actualizó las descripciones de cada función con un comentario, y arreglos menores en las mismas.

--- funciones.h
+++ funciones.h
@@ -1,48 +1,48 @@
 //Funciones o Prototipos
 /*
-* Nombre de la funciÃ³n: paciente.
-* Tipo de funciÃ³n: int
-* ParÃ¡metros: no tiene.
+* Nombre de la función: paciente.
+* Tipo de función: int
+* Parámetros: no tiene.
 * Dato de retorno: datos almacenados en variantes como nombre,rut,edad,etc... ;
-* DescripciÃ³n de la funciÃ³n: La funciÃ³n paciente() es de recepciÃ³n de datos, datos para reconocimiento de paciente y futura
-localizaciÃ³n del misma, ya que todo esto serÃ¡ acompaÃ±ado con su receta de la funciÃ³n medicamentos.
+* Descripción de la función: La función paciente() es de recepción de datos, datos para reconocimiento de paciente y futura
+localización del misma, ya que todo esto será acompañado con su receta de la función medicamentos.
 
-* Nombre de la funciÃ³n: medicamentos()
-* Tipo de funciÃ³n: int.
-* ParÃ¡metros: No tiene.
-* Dato de retorno: Guarda todo las variables en un .txt y ademÃ¡s se inserta la informaciÃ³n en una Lista.
-* DescripciÃ³n de la funciÃ³n: La funciÃ³n medicamentos() se encarga de solicitar el medicamento a suministrar,dosis y
-calcular la concentraciÃ³n, luego se guarda en un .txt para luego pasar a la siguiente funciÃ³n inserta().
+* Nombre de la función: medicamentos()
+* Tipo de función: int.
+* Parámetros: No tiene.
+* Dato de retorno: Guarda todo las variables en un .txt y además se inserta la información en una Lista.
+* Descripción de la función: La función medicamentos() se encarga de solicitar el medicamento a suministrar,dosis y
+calcular la concentración, luego se guarda en un .txt para luego pasar a la siguiente función inserta().
 
-* Nombre de la funciÃ³n: inserta()
-* Tipo de funciÃ³n: void.
-* ParÃ¡metros: â€œxâ€ de tipo char(aunque tipoDato puede variar).
-* â€œpâ€ de tipo int y lista de tipo Lista.
+* Nombre de la función: inserta()
+* Tipo de función: void.
+* Parámetros: “x” de tipo char(aunque tipoDato puede variar).
+* “p” de tipo int y lista de tipo Lista.
 * Dato de retorno: Retorna el siguiente punto en nuestra lista;
-* DescripciÃ³n de la funciÃ³n: La funciÃ³n inserta() se encarga de tomar toda la informaciÃ³n puesta en la receta,
-e insertarlo en una lista de tipo Lista, ya que en el caso de querer descartar el .txt, se podrÃ­a realizar una funciÃ³n
-de bÃºsqueda por rut,nombre u otra variable almacenada.
+* Descripción de la función: La función inserta() se encarga de tomar toda la información puesta en la receta,
+e insertarlo en una lista de tipo Lista, ya que en el caso de querer descartar el .txt, se podría realizar una función
+de búsqueda por rut,nombre u otra variable almacenada.
 /*
-* Nombre de la funciÃ³n: primero()
-* Tipo de funciÃ³n: int.
-* ParÃ¡metros: â€œlistaâ€ de tipo Lista.
+* Nombre de la función: primero()
+* Tipo de función: int.
+* Parámetros: “lista” de tipo Lista.
 * Dato de retorno: El primero, en este caso el numero 1.
-* DescripciÃ³n de la funciÃ³n: La funciÃ³n primero() se encarga de situar la informaciÃ³n en el primer espacio de una lista
+* Descripción de la función: La función primero() se encarga de situar la información en el primer espacio de una lista
 de tipo Lista,y de no haber uno ,crear ese espacio.
 
 /*
-* Nombre de la funciÃ³n: vacia().
-* Tipo de funciÃ³n: bool.
-* ParÃ¡metros: lista de tipo Lista.
-* Dato de retorno: una lista vacÃ­a.
-* DescripciÃ³n de la funciÃ³n: La funciÃ³n vacia(), se encarga de revisar si nuestra Lista estÃ¡ vacÃ­a o no, para que no hayan
+* Nombre de la función: vacia().
+* Tipo de función: bool.
+* Parámetros: lista de tipo Lista.
+* Dato de retorno: una lista vacía.
+* Descripción de la función: La función vacia(), se encarga de revisar si nuestra Lista está vacía o no, para que no hayan
 errores de funcionamiento con las otras funciones.
 
-* Nombre de la funciÃ³n: fin().
-* Tipo de funciÃ³n: int.
-* ParÃ¡metros: lista de tipo Lista.
-* Dato de retorno: ubicaciÃ³n del final de nuestra lista.
-* DescripciÃ³n de la funciÃ³n: La funciÃ³n lista(), se encarga de revisar el final de nuestra lista.
+* Nombre de la función: fin().
+* Tipo de función: int.
+* Parámetros: lista de tipo Lista.
+* Dato de retorno: ubicación del final de nuestra lista.
+* Descripción de la función: La función lista(), se encarga de revisar el final de nuestra lista.
 */
 #include <Structpacientes.h>
 int paciente();
@@ -51,4 +51,3 @@ void inserta (tipoDato x, int p, Lista &lista); //lista para insertar la "receta
 int primero(Lista lista);
 bool vacia(Lista lista);
 int fin(Lista lista);
-
